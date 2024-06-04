90a40af8b669c73ac1f068783b13a376334a766d
ALUNO <ALUNO@10.85.198.7>
2024-06-04 16:02:27 -0300

    Commit project Chicken01

---
+++ src/Chickens01.java
@@ -0,0 +1,15 @@
+public class Chickens01 {
+    public static void main(String[] args) {
+        int count=15; // contagem inicial de ovos 
+        int a, b, c; // segunda, terça e quarta
+        a = count;  // contagem de ovos  de segunda com tres galinhas 
+        b = count + 5; // contagem de ovos de terça com mais uma galinha 
+        c = count - 5; //contagem de ovos de quarta comm menos duas galinhas
+       
+     
+        
+        System.out.println("Segunda-feira = " + a +" , ");
+        System.out.println("Terca-feira = " + b + " , " );
+        System.out.println("Quarta- feira = " + c + " , ");
+    }   //Segunda = 15 ovos | terça = 20 ovos | quarta 10 0vos 
+}
